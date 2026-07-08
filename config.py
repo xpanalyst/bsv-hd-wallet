24c7189af1aa59df979297f46c0b606e3d5f400b
xp_analyst <80833764+xpanalyst@users.noreply.github.com>
2026-07-08 07:50:37 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+ITERATIONS = 200_000
+KEYSTORE_FILE = "wallet_keystore.json"
+DB_FILE = "wallet.db"
+
+NETWORK = "test"  # lub "main" dla mainnet
+WOC_BULK_BALANCE_URL = f"https://api.whatsonchain.com/v1/bsv/{NETWORK}/addresses/confirmed/balance"
